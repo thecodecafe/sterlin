3ff8b0b0b2fdeae73e1971374853ab107129c8ed
Daniel Barde <mrbarde2k@gmail.com>
2019-07-11 12:49:22 +0100

    Refactor response object generator

--- __tests__/utils/Response.test.js
+++ __tests__/utils/Response.test.js
@@ -13,17 +13,24 @@ const {
 const mockResponse = () => {
   const res = {};
   res.status = (code) => {
+    // chane status to the code passed
     res.status = code;
+    
+    // return response object
     return res;
   };
   res.json = (obj) => {
+    // remove json and status from object if found
     delete obj.json;
-    Object.keys(obj).map(i => {
-      if(i === 'json') return;
-      res[i] = obj[i];
-    })
+    delete obj.status;
+
+    // go over all object
+    Object.keys(obj).forEach(i => res[i] = obj[i]);
+
+    // return response object
     return res;
   };
+  // retunr response object
   return res;
 };
 
