ca1110795908357d8fe81805c2a9e7cb784d7590
Daniel Barde <mrbarde2k@gmail.com>
2019-07-13 23:35:58 +0100

    Remove unnecessary rules and correct is date rule logic.

--- utils/Rules.util.js
+++ utils/Rules.util.js
@@ -17,20 +17,6 @@ const isNunber = value => {
   return re.test(value);
 };
 
-const is24HourTime = value => {
-  var re = /^\d\d:\d\d(:\d\d)?$/;
-  return re.test(value);
-};
-
-const is12HourTime = value => {
-  var re = /^\d\d:\d\d(am|pm|AM|PM)?$/;
-  return re.test(value);
-};
-
-const isTime = value => {
-  return is24HourTime(value) || is12HourTime(value);
-};
-
 const isRequired = value => {
   if (value === undefined) return false;
   if (value === null) return false;
@@ -45,58 +31,9 @@ const isRequiredWith = parent => value => {
   return isRequired(value);
 };
 
-const isTrue = value => {
-  return value === true || value === 1;
-};
-
-const isFalse = value => {
-  return value === false || value === 0;
-};
-
-const maxLength = length => value => {
-  if (typeof value !== 'string') return false;
-  return value && value.length <= length;
-};
-
-const minLength = length => value => {
-  if (typeof value !== 'string') return false;
-  return value && value.length >= length;
-};
-
-const isBetweenLength = (min, max) => value => {
-  if (typeof value !== 'string') return false;
-  return minLength(min)(value) && maxLength(max)(value);
-};
-
-const isMatch = first => value => {
-  return value === first;
-};
-
-const isNotMatch = first => value => {
-  return value !== first;
-};
-
-const max = limit => value => {
-  return value <= limit;
-};
-
-const min = limit => value => {
-  return value >= limit;
-};
-
-const isBetween = (min, max) => value => {
-  if (typeof value !== 'string') return false;
-  return min(min)(value) && max(max)(value);
-};
-
-const isIn = list => value => {
-  return list.indexOf(value) !== -1 ? true : false;
-};
-
 const isDate = value => {
-  return String(new Date(value).getTime())
-    .trim()
-    .toLowerCase() !== 'invalid date';
+  const dt = new Date(value).toUTCString();
+  return /invalid/ig.test(dt) ? false : true;
 };
 
 const isAfterDate = start => {
@@ -144,25 +81,11 @@ const isBetweenDate = (start, end) => {
 module.exports = {
   isRequired,
   isRequiredWith,
-  isTrue,
-  isFalse,
   isDate,
   isBeforeDate,
   isAfterDate,
   isBetweenDate,
-  isIn,
-  min,
-  max,
-  isBetween,
-  minLength,
-  maxLength,
-  isBetweenLength,
-  is12HourTime,
-  is24HourTime,
-  isTime,
   isEmail,
   isNunber,
-  isMatch,
-  isNotMatch,
   isURL
 };
