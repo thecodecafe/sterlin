e5d353202a832f5a901dafde569520e43f2830a0
Daniel Barde <mrbarde2k@gmail.com>
2019-07-15 00:08:08 +0100

    Add entrypoint

---
+++ index.js
@@ -0,0 +1,35 @@
+require('./configs/dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { mongoURI } = require('./configs/app');
+const { notFound } = require('./utils/Response.util');
+const port = process.env.PORT || 3000;
+
+// initialize app
+const app = express();
+
+// initialize body parser
+app.use(bodyParser.json());
+
+// connect to database
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("Database connected..."))
+  .catch(err => console.error(err));
+
+// setup routes
+app.use('/auth', require("./routes/Auth"));
+app.use('/seasons', require("./routes/Seasons"));
+app.use('/teams', require("./routes/Teams"));
+app.use('/fixtures', require("./routes/Fixtures"));
+
+app.use((req, res) => {
+  return notFound(
+    res,
+    'Resource has either been moved or does not exist.'
+  );
+});
+
+// start server
+app.listen(port, () =>  console.log(`Server started on port ${port}.`));
